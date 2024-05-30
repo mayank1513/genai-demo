ce3085cfa4296b24d107b167e1e1b5d2cdd067f5
Mayank Kumar Chaudhari <mayank.srmu@gmail.com>
2024-05-30 15:45:24 +0530

    Add ai-models

---
+++ src/constants/ai-models.ts
@@ -0,0 +1,6 @@
+export enum ReplicateAIModels {
+  TextToImage = "2b017d9b67edd2ee1401238df49d75da53c523f36e363881e057f5dc3ed3c5b2",
+  BgRemover = "fb8af171cfa1616ddcf1242c093f9c46bcada5ad4cf6f2fbe8b81b330ec5c003",
+  ObjRemover = "153b0087c2576ad30d8cbddb35275b387d1a6bf986bda5499948f843f6460faf",
+  FaceRestorer = "297a243ce8643961d52f745f9b6c8c1bd96850a51c92be5f43628a0d3e08321a",
+}
